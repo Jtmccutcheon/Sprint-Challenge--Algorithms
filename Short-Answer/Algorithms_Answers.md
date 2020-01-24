a7dc68ab1b2497c0127a3b67a63a7c430a492e53
jtmccutcheon <jt.mccutcheon@live.com>
2020-01-24 17:19:47 -0500

    add other files

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -1,15 +1,31 @@
-#### Please add your answers to the ***Analysis of  Algorithms*** exercises here.
+#### Please add your answers to the **_Analysis of Algorithms_** exercises here.
 
 ## Exercise I
 
-a)
+a) O(n) because the loop shows n _ n _ n and a increases n \* n make the solution iterate o(n) times
 
+b) o(n^2) becuase there are 2 checks for n and is in a for loop of n which i think makes o(n^2)
 
-b)
-
-
-c)
+c) o(n) because even though theres recursion n stays a constant
 
 ## Exercise II
 
+this represents our building
+h break
+g break
+f break
+e notbreak
+d notbreak
+c notbreak
+b notbreak
+a notbreak
 
+```
+def eggSaftey(n):
+    if n < f:
+        return 'egg will not break'
+    else:
+        return 'egg will break'
+```
+
+o(n) because n doesnt change as we go through the algorithm
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,21 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+
+    # if length of a word is less than or equal to one we know it cant have "th"
+    if len(word) <= 1:
+        return 0
+    # check if the first two characters are "th"
+    if word[0] + word[0+1] == 'th':
+        # we can add one because theres one instance of "th"
+        # recursively call our function to check the remaining string
+        # after the frist 2 characters
+        return 1 + count_th(word[2:])
+    else:
+        # if the first two charactesr are not "th"
+        # move down the string one posistion and check
+        # the next two characters
+        return count_th(word[1:])
